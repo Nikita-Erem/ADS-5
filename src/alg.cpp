5fe35b708c4a7d692a26c2adae8d4af8cff6ecc6
Nikita-Erem <160235350+Nikita-Erem@users.noreply.github.com>
2024-04-22 13:35:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,72 +3,78 @@
 #include <map>
 #include "tstack.h"
 
+TStack<char, 100> stack1;
+TStack<int, 100> stack2;
+
 int Priority(char operand) {
-    if (operand == '+' || operand == '-') return 1;
-    if (operand == '*' || operand == '/') return 2;
-    return 0;
+  if (operand == '+' || operand == '-') return 1;
+  if (operand == '*' || operand == '/') return 2;
+  return 0;
 }
-
-bool isOperator(char c) {
-    return (c == '+' || c == '-' || c == '*' || c == '/');
-}
-
 std::string infx2pstfx(std::string inf) {
- std::string postfix;
-    TStack<char, 100> stack;
-    for (char c : inf) {
-        if (isdigit(c)) {
-            postfix = postfix + c + ' ';
-        } else if (c == '(') {
-            stack.push(c);
-        } else if (isOperator(c)) {
-            while (!stack.isEmpty() && Priority(stack.get()) >= Priority(c)) {
-                postfix = postfix + stack.get() + ' ';
-                stack.pop();
-            }
-            stack.push(c);
-        } else if (c == ')') {
-            while (!stack.isEmpty() && stack.get() != '(') {
-                postfix = postfix + stack.get() + ' ';
-                stack.pop();
-            }
-            stack.pop();
-        }
-    }
-    while (!stack.isEmpty()) {
-        postfix = postfix + stack.get() + ' ';
+  std::string postfix;
+  TStack<char, 100> stack;
+  for (char c : inf) {
+    if (isdigit(c)) {
+      postfix = postfix + c + ' ';
+    } else if (c == '(') {
+      stack.push(c);
+    } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+      while (!stack.isEmpty() && Priority(stack.show()) >= Priority(c)) {
+        postfix = postfix + stack.show() + ' ';
         stack.pop();
+      }
+      stack.push(c);
+    } else if (c == ')') {
+      while (!stack.isEmpty() && stack.show() != '(') {
+        postfix = postfix + stack.show() + ' ';
+        stack.pop();
+      }
+      stack.pop();
     }
-    if (!postfix.empty()) {
-        postfix.pop_back();
-    }
-    return postfix;
+  }
+  while (!stack.isEmpty()) {
+    postfix = postfix + stack.show() + ' ';
+    stack.pop();
+  }
+  if (!postfix.empty()) {
+    postfix.pop_back();
+  }
+  return postfix;
 }
-int eval(std::string post) {
-    TStack<int, 100> stack;
-    std::string Number;
-    for (char c : post) {
-        if (isdigit(c)) {
-            Number += c;
-        } else if (Number != "") {
-            stack.push(std::atoi(Number.c_str()));
-            Number = "";
-        }
-        if (isOperator(c)) {
-            int a = stack.get();
-            stack.pop();
-            int b = stack.get();
-            stack.pop();
-            if (c == '+') {  
-                  stack2.add((a + b));
-                } else if (c == '-') {
-                 stack2.add((a - b));
-                 } else if (c == '*') {
-                 stack2.add((a * b));
-                } else if (c == '/') {
-                 stack2.add((a / b));
+
+int eval(std::string pref) {
+  std::string strNumber = "";
+  for (char c : pref) {
+    if (c != ' ' && c != '+' && c != '-' && c != '*' && c != '/') {
+      strNumber += c;
+    }
+    if (c == ' ') {
+      if (!strNumber.empty()) {
+        int num = std::stoi(strNumber);
+        stack2.push(num);
+        strNumber = "";
+      }
+    }
+    if (c == '+' || c == '-' || c == '*' || c == '/') {
+      int i = stack2.show();
+      stack2.pop();
+      int j = stack2.show();
+      stack2.pop();
+
+      if (c == '+') {
+        stack2.push((j + i));
+      } else if (c == '-') {
+        stack2.push((j - i));
+      } else if (c == '*') {
+        stack2.push((j * i));
+      } else if (c == '/') {
+        stack2.push((j / i));
       }
     }
   }
+
+  return stack2.show();
+}
     return stack.get();
 }
